b27fa452b509535a38473296529b8d5fa67488e7
PrateekGithub786 <75919841+PrateekGithub786@users.noreply.github.com>
2021-10-07 16:39:34 +0530

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -55,7 +55,7 @@ var gameConfig={
 
 noseX = "";
 noseY = "";
-gameStatus = "";
+GameStatus = "";
 
 function game(){
   console.log("Nose X = " + noseX + " Nose Y = " + noseY);
@@ -73,7 +73,7 @@ function game(){
     fill(255, 255, 255);
     textSize(40);
     textAlign(CENTER);
-    text("Press Any Arrow Keys to Start and Play ", gameConfig.screenX/2, gameConfig.screenY/2);
+    text("Press Start Button to Start and Play ", gameConfig.screenX/2, gameConfig.screenY/2);
     textSize(40);
 
     stroke(255);
@@ -122,12 +122,12 @@ function game(){
 // change game status if any key is pressed
 
 function start_game(){
-  gameStatus = "start";
+  GameStatus = "start";
   document.getElementsById("status").innerHTML = "Game is Loading";
 }
 
 function changeGameStatud(character){
-  if((keyDown(control.up) ||keyDown(control.left)||keyDown(control.right) )&& gameConfig.status==="start") {
+  if(noseX != "" && GameStatus =="start" && gameConfig.status==="start") {
     world_start.play();
     initializeCharacterStatus(mario);
     gameConfig.status= "play";
@@ -291,13 +291,13 @@ function autoControl(character){
 function manualControl(character){
   
   if(character.live){
-    if(keyDown(control.left)){
+    if(noseX < 400){
       character.velocity.x-=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(-1);
     }
 
-    if(keyDown(control.right)){
+    if(noseX > 400){
       character.velocity.x+=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(1);
@@ -312,7 +312,7 @@ function manualControl(character){
 
 /* Movements of character */
 function jumping(character){
-	if( (keyWentDown(control.up)&&character.live) || (touchIsDown&&character.live) ){
+	if( (noseY > 200&&character.live) || (touchIsDown&&character.live) ){
 		character.velocity.y+=gameConfig.jump;
 	}
 }
